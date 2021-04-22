0abcf10be69ba44c4fc924721aed11ec58c14bb4
sssemion <ssemi.on@yandex.ru>
2021-04-23 01:17:31 +0500

    Удаление сервера через ajax-запрос

--- app/ajax_controllers.py
+++ app/ajax_controllers.py
@@ -1,9 +1,15 @@
 from flask import Blueprint, jsonify
 
+from app.exceptions import ResourceNotFound
+from app.services.mc_servers import delete_server
+
 ajax = Blueprint("ajax", __name__)
 
 
-@ajax.route("/server/delete/<int:server_id>")
-def delete_server(server_id):
-    return jsonify({"success": True})
-
+@ajax.route("/server/delete/<int:server_id>", methods=["POST"])
+def delete_server_controller(server_id):
+    try:
+        delete_server(server_id)
+        return jsonify({"success": True})
+    except ResourceNotFound as e:
+        return jsonify({"success": False, "message": str(e)})
--- app/services/mc_servers.py
+++ app/services/mc_servers.py
@@ -2,7 +2,7 @@ from flask_login import current_user
 
 from app.data.db_session import create_session
 from app.data.models import MCServer, User
-from app.exceptions import ServerAlreadyAdded, ServerConnectionError
+from app.exceptions import ServerAlreadyAdded, ServerConnectionError, ResourceNotFound
 from app.services.minecraft import check_connection
 
 
@@ -20,3 +20,14 @@ def create_server(name, host, rcon_port, rcon_password, nickname, make_active=Tr
         session.commit()
         if make_active:
             user.active_mc_server = server.id
+
+
+def delete_server(server_id):
+    with create_session() as session:
+        server = session.query(MCServer).get(server_id)
+        if server is None:
+            raise ResourceNotFound
+        if server.owner.active_mc_server == server.id:
+            server.owner.active_mc_server = None
+        server.owner.mc_servers.remove(server)
+        session.delete(server)
